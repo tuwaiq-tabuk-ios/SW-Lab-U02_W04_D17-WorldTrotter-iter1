b75ce5a1efb5db2e2ac01723a261899001098683
Shorouq AlAnzi <shorouqalanzi@MacBook-Air-Shorouq.local>
2021-10-25 21:43:38 +0300

    Create project

---
+++ WorldTrotter-ShuruqAlanazi-iter2/WorldTrotter-ShuruqAlanazi-iter2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter-ShuruqAlanazi-iter2
+//
+//  Created by Shorouq AlAnzi on 19/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
