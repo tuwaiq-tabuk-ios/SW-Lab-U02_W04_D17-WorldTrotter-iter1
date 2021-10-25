2aff351d1983bb9e31112b09eef78b093626d36a
Shorouq AlAnzi <shorouqalanzi@MacBook-Air-Shorouq.local>
2021-10-26 00:08:48 +0300

    new WorldTrotter project

--- WorldTrotter-ShuruqAlanazi-iter2/WorldTrotter-ShuruqAlanazi-iter2/ViewController.swift
+++ WorldTrotter-ShuruqAlanazi-iter2/WorldTrotter-ShuruqAlanazi-iter2/ViewController.swift
@@ -12,8 +12,15 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    let gradientLayer = CAGradientLayer()
+                gradientLayer.frame = self.view.bounds
+                gradientLayer.colors = [UIColor.purple.cgColor,
+                                        UIColor.blue.cgColor,
+                                        UIColor.green.cgColor,
+                                        UIColor.yellow.cgColor,
+                                        UIColor.red.cgColor]
+                self.view.layer.insertSublayer(gradientLayer, at: 0)  // << here !!
+            }
   }
 
-
-}
 
