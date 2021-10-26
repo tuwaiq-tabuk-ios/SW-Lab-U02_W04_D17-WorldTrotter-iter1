61c6be3f12e31b3a393297d35d99a7a010f4c455
A A <aa@MacBook-Pro-A.local>
2021-10-26 11:33:57 +0300

    WorldTrotter

---
+++ WorldTrotter-Programmatically-Areej_Alanze-iter1/WorldTrotter-Areej_Alanze-Manually/WorldTrotter-Areej_Alanze-iter1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter-Areej_Alanze-iter1
+//
+//  Created by A A on 19/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
