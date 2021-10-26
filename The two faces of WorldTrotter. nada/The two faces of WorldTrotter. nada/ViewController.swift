66bfb21517ddc53753878108f2e2a02e5f57feaf
apple <apple@MacBook-Pro-macbook.local>
2021-10-26 16:47:26 +0300

    compler

---
+++ The two faces of WorldTrotter. nada/The two faces of WorldTrotter. nada/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The two faces of WorldTrotter. nada
+//
+//  Created by apple on 20/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
