d78930d0b2e379eff2beaa72f4ba9702b21e2833
rasha <rasha1418sh@icloud.com>
2021-10-26 14:07:52 +0300

    update

---
+++ WorldTrotter-"Rasha Aiad"-iter1/WorldTrotter-"Rasha Aiad"-iter1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WorldTrotter-"Rasha Aiad"-iter1
+//
+//  Created by rasha  on 20/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let gradientLayer = CAGradientLayer()
+    gradientLayer.frame = view.bounds
+    gradientLayer.colors = [UIColor.purple.cgColor,
+                                                                                                    UIColor.blue.cgColor, UIColor.green.cgColor, UIColor.yellow.cgColor, UIColor.orange.cgColor, UIColor.red.cgColor]
+    
+    self.view.layer.insertSublayer(gradientLayer, at: 0)
+    // Do any additional setup after loading the view.
+  }
+  
+}
