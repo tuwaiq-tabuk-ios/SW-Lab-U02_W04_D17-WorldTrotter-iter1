3c7df233be62a93f873ce596aad74d19dfd6b6a8
Maha S <mahas@Mahas-MacBook-Air.local>
2021-10-26 11:05:57 +0300

    WorldTrotter iter1

---
+++ WorldTrotter-Programmatically-Maha_Nasser-iter1/WorldTrotter-MahaNasser-iter1-Challenges(Bronz&Silver)/WorldTrotter-MahaNasser-iter1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  WorldTrotter-MahaNasser-iter1
+//
+//  Created by Maha S on 25/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let gradientLayer = CAGradientLayer()
+    
+    gradientLayer.colors = [UIColor.purple.cgColor,
+                            UIColor.blue.cgColor,
+                            UIColor.green.cgColor,
+                            UIColor.yellow.cgColor,
+                            UIColor.orange.cgColor,
+                            UIColor.red.cgColor]
+    
+    gradientLayer.frame.size = view.frame.size
+    
+    view.layer.insertSublayer(gradientLayer, at:0)
+  }
+
+
+}
+
---
+++ WorldTrotter-Programmatically-Maha_Nasser-iter1/WorldTrotter-MahaNasser-iter1/WorldTrotter-MahaNasser-iter1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter-MahaNasser-iter1
+//
+//  Created by Maha S on 25/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+
+
+}
+
