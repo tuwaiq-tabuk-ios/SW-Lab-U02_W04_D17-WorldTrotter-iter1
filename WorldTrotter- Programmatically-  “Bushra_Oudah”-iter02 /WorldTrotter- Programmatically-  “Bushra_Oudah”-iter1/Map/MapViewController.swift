e98a7098507e056cb06dc2fc14ad3bfdd2e64249
Bushra alatwi <bushraalatwi@Bushras-MacBook-Air.local>
2021-10-27 22:30:48 +0300

    completed

--- WorldTrotter- Programmatically-  “Bushra_Oudah”-iter02 /WorldTrotter- Programmatically-  “Bushra_Oudah”-iter1/Map/MapViewController.swift
+++ WorldTrotter- Programmatically-  “Bushra_Oudah”-iter02 /WorldTrotter- Programmatically-  “Bushra_Oudah”-iter1/Map/MapViewController.swift
@@ -8,13 +8,16 @@
 import Foundation
 
 import UIKit
+import MapKit
+
+
 class MapViewController: UIViewController {
+
+       
+  
   override func viewDidLoad() {
-    
     super.viewDidLoad()
     
   print("Map ViewController loaded its View.")
    }
 }
-
-
