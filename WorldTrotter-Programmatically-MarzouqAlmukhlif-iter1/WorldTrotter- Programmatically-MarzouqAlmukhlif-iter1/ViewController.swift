7cf9302358924bba0740abbea5f2aa90d5dbda17
Marzouq Almukhlif <mhster_nice@Marzouqs-MacBook-Pro.local>
2021-10-28 14:07:12 +0300

    Extra Project WorldTrotter-Programmatically

---
+++ WorldTrotter-Programmatically-MarzouqAlmukhlif-iter1/WorldTrotter- Programmatically-MarzouqAlmukhlif-iter1/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  WorldTrotter- Programmatically-MarzouqAlmukhlif-iter1
+//
+//  Created by Marzouq Almukhlif on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Add first View
+        let firstFrame = CGRect(x: 160,
+                                y: 240,
+                                width: 100,
+                                height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.systemBlue
+        view.addSubview(firstView)
+    
+    // Add second View
+    let secondFrame = CGRect(x: 20,
+                             y: 30,
+                             width: 50,
+                             height: 50)
+    let secondView = UIView(frame: secondFrame)
+    secondView.backgroundColor = UIColor.systemGreen
+    firstView.addSubview(secondView)
+    
+    
+    
+    
+    
+  }
+  
+  
+}
+
