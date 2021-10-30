3a4ac8afd4edc4ba8b61d3c0bb68f2ef9c89a948
Aisha Ali <aamiss1496@gmail.com>
2021-10-30 15:07:57 +0300

    worldtrotter

---
+++ WorldTrotter- Aisha_Ali-iter1 Gold Challenge/WorldTrotter- Programmatically-  Aisha_Ali-iter1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WorldTrotter- Programmatically-  Aisha_Ali-iter1
+//
+//  Created by Aisha Ali on 10/24/21.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let gradientLayer = CAGradientLayer()
+    gradientLayer.colors = [UIColor(red: (64/255.0), green: (54/255.0), blue: (105/255.0), alpha: 7.0).cgColor,
+                            UIColor.orange.withAlphaComponent(0.5).cgColor]
+        gradientLayer.frame.size = view.frame.size
+        view.layer.insertSublayer(gradientLayer, at:0)
+
+
+  }
+  
+
+}
+
